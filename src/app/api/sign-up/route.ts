6ceaef7e0b609eab7fdbc6ee16ee8086f416eb69
Rudra Pratap Singh <130541940+rudrapratap63@users.noreply.github.com>
2025-12-28 23:54:09 +0530

    fix(sign-up): correct typo in password variable name

--- src/app/api/sign-up/route.ts
+++ src/app/api/sign-up/route.ts
@@ -7,7 +7,7 @@ import { sendVerificationEmail } from "@/helpers/sendVerificationEmail";
 export async function POST(request: Request){
    await dbConnect();
    try {
-      const {username, email, passowrd} = await request.json();
+      const {username, email, password} = await request.json();
       
       const existingUserVerifiedByUsername = await UserModel.findOne({
          username,
@@ -31,14 +31,14 @@ export async function POST(request: Request){
                message: "User already exist with this email"
             },{status: 400})
          }else {
-            const hashedPassword = await bcrypt.hash(passowrd, 10);
+            const hashedPassword = await bcrypt.hash(password, 10);
             existingUserByEmail.password = hashedPassword;
             existingUserByEmail.verifyCode = verifyCode;
             existingUserByEmail.verifyCodeExpiry = new Date(Date.now() + 3600000)
             await existingUserByEmail.save();
          }
       }else{
-         const hashedPassword = await bcrypt.hash(passowrd, 10);
+         const hashedPassword = await bcrypt.hash(password, 10);
          const expiryDate = new Date();
          expiryDate.setHours(expiryDate.getHours() + 1)
 
